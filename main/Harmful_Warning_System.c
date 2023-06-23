758fc97ebf9f4685e7e84ea8c84dfd76f2b7d178
ragilalfarizi <alfariziragill@gmail.com>
2023-06-23 09:31:29 +0700

    initial commit

---
+++ main/Harmful_Warning_System.c
@@ -0,0 +1,14 @@
+#include <driver/gpio.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <stdio.h>
+
+#define ledMerah GPIO_NUM_4
+#define ledHijau GPIO_NUM_5
+#define Buzzer GPIO_NUM_23
+#define sensorDHT11 GPIO_NUM_15
+
+void app_main(void) {
+  while (1) {
+  }
+}
